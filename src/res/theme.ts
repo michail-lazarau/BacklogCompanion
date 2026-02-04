822f87d2771ddab4e9438e4a2cf61300ab529b75
Mikhail Lazarau <m.lazarau@godeltech.com>
2026-02-04 21:20:18 +0100

    feat: theme update

--- src/res/theme.ts
+++ src/res/theme.ts
@@ -5,6 +5,7 @@ export const colors = {
   containerBackground: '#F8FAFC',
   surface: '#E2E8F0', // everything on top of containerBackground: for component surfaces like cards, badges, and dividers
   title: '#1A202C', // '#1A202C'
+  subtitle: '#4A5568',
   textFootnote: '#718096',
   textError: '#FF3B30',
   textPressableComponent: '#FFFFFF',
